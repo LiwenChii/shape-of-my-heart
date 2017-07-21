3025115e2f2613f90ef9bfca6760b949af53d0b9
LiwenChii <328957392@qq.com>
2017-07-21 10:58:49 +0800

    add huffman-trees

---
+++ huffman-trees.scm
@@ -0,0 +1,70 @@
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? object) (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+(define shit (make-code-tree (list 'leaf 'a 4) (list 'leaf 'a 4)))
+(make-code-tree shit (list 'leaf 'b 5))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge leaf-pairs)
+  (cond ((= 0 (length leaf-pairs))
+         '())
+        ((= 1 (length leaf-pairs))
+         (car leaf-pairs))
+        (else
+         (let ((new-sub-tree (make-code-tree (car leaf-pairs)
+                                             (cadr leaf-pairs)))
+               (remained-leaf-pairs (cddr leaf-pairs)))
+           (successive-merge (adjoin-set new-sub-tree remained-leaf-pairs))))))
+
+(generate-huffman-tree '((A 4) (B 2) (C 1) (D 1)))
