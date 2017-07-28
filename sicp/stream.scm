9a3a604db95a8843d737aa7cd1a317ea19bb1d36
LiwenChii <328957392@qq.com>
2017-07-28 16:58:37 +0800

    Update stream.scm

--- sicp/stream.scm
+++ sicp/stream.scm
@@ -1,5 +1,33 @@
 (define false #f)
 (define true #t)
+(define the-empty-stream '())
+(define stream-null? null?)
+(define nil '())
+
+(define (square x)
+  (* x x))
+
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+(define (divides? a b) (= (remainder b a) 0))
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
 
 (define (stream-ref s n)
   (if (= n 0)
@@ -35,22 +63,46 @@
                  result)
           result))))
 
-(define (delay exp)
-  (memo-proc (lambda () exp)))
+(define-syntax delay
+  (syntax-rules ()
+    ((_ exp) (memo-proc (lambda () exp)))))
+
 
 (define (force1 delayed-object)
   (delayed-object))
 
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a b) (cons a (delay b)))))
 
-(define (cons-stream a b)
-  (cons a (delay b)))
 
 (define (stream-car stream) (car stream))
 (define (stream-cdr stream) (force1 (cdr stream)))
 
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
 
 (define z (cons-stream 3 5))
 (stream-car z)
 (stream-cdr z)
+(stream-enumerate-interval 1000 100000)
 
 
+(stream-car
+ (stream-cdr
+  (stream-filter prime?
+                 (stream-enumerate-interval 1000000 100000000))))
+
+(car (cdr (filter prime? (enumerate-interval 1000000 100000000))))
