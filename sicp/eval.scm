667eaa025b4df2500aaaba1e71f50668c81394b3
LiwenChii <328957392@qq.com>
2017-07-29 21:28:32 +0800

    update eval

--- sicp/eval.scm
+++ sicp/eval.scm
@@ -72,6 +72,8 @@ e
 (definition? e)
 (definition-value e)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
@@ -82,6 +84,25 @@ e
   (if (true? (eval (if-predicate exp) env))
       (eval (if-consequent exp) env)
       (eval (if-alternative exp) env)))
+
+(define (eval-sequence exps env)
+  (cond ((lasst-exp? exps)
+         (eval (first-exp exps) env))
+        (else
+         (eval (first-exp exps) env)
+         (eval-sequence (rest-exps exps) env))))
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval (assignment-value exp) env)
+                       env)
+  'ok)
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+    (eval (definition-value exp) env)
+    env)
+  'ok)
 
 (define (apply procedure arguments)
   (cond ((primitive-procedure? procedure)
